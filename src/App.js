c54f7c90c215b1f5f0fba1f0a7e445ee99fcb920
MrEmilCavic <mr.emil.cavic@gmail.com>
2024-05-07 08:27:11 +0200

    commit init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import SearchBar from './components/SearchBar.js';
+import SearchResult from './components/SearchResult.js';
+import Playlist from './components/Playlist.js';
+import TrackList from './components/TrackList.js';
+import Track from './components/Track.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <SearchBar />
+      <SearchResult />
+      <Playlist />
+      <TrackList />
+      <Track />
     </div>
+    
   );
 }
 
