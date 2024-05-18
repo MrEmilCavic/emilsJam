00042199971878c5c5e81c24bdb2573cdb9b9fa4
MrEmilCavic <164026541+MrEmilCavic@users.noreply.github.com>
2024-05-18 14:44:13 +0200

    Update App2.js changed the link to the new adress

--- src/App2.js
+++ src/App2.js
@@ -3,9 +3,9 @@ import App from './App.js';
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 
 const router = createBrowserRouter(createRoutesFromElements(
-        <Route path='/emilsjam' element={<App />}>
+        <Route path='/emilsJam' element={<App />}>
         <Route path='*' element={<App />} />
-        <Route path='/emilsjam/callback' element={<App />} />
+        <Route path='/emilsJam/callback' element={<App />} />
         </Route>
 ));
 
